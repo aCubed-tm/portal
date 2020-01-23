74ce5be2f060300d921aaa080dda3d7008181dcd
Lenny Kraaijenhof <hi@lenny.kr>
2020-01-23 13:56:33 +0100

    Add basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
+<style lang="scss" scoped>
+  @import './assets/styles/main';
+
+  #app {
+      min-height: 100vh;
+      min-height: calc(var(--vh, 1vh) * 100);
+      @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+          height: 100vh;
+          height: calc(var(--vh, 1vh) * 100);
+          align-items: center;
+      }
+  }
+</style>
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,44 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
+
+/**
+ * Lazy-loads view components, but with better UX. A loading view
+ * will be used if the component takes a while to load, falling
+ * back to a timeout view in case the page fails to load. You can
+ * use this component to lazy-load a route with:
+ *
+ * component: () => lazyLoadView(import('@views/my-view'))
+ *
+ * @param {AsyncView} The view component
+ */
+const lazyLoadView = (AsyncView) => {
+  const AsyncHandler = () => ({
+      component: AsyncView,
+      delay: 400,
+      timeout: 10000,
+  });
+
+  return Promise.resolve({
+      functional: true,
+      render(h, { data, children }) {
+          return h(AsyncHandler, data, children);
+      },
+  });
+};
+
+
+const loadView = (view) => {
+  const path = `@/views/${view}.vue`;
+  return () => lazyLoadView(import(path));
+};
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'account',
+    component: loadView('profile/show'),
   },
 ];
 
