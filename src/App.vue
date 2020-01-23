7d1c03b1c06afa3dcc4308d12b8fbe3f836ccca7
Jasey <Jwaegebaert@gmail.com>
2020-01-23 13:04:34 +0100

    Gradient bgc

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/registerPassword">Reg-pass</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background: rgb(11,4,37);
+  background: radial-gradient(200% 150% ellipse at 50% 215%, rgba(190,100,154,1), rgba(11,4,37,1));
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/register/RegPassword.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="register-password">
+        <p>This is the register password view</p>
+    </div>
+</template>
+
+<script>
+// * Imports
+
+// * Export
+export default {
+  name: 'Register-password',
+
+};
+</script>
+
+<style lang="scss">
+</style>
