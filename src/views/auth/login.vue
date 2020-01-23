1e5e7ae189676963b1447776a903c8f910f7429e
Lenny Kraaijenhof <hi@lenny.kr>
2020-01-23 20:19:23 +0100

    Move login components

---
+++ src/views/auth/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Sign in to Portal</h1>
+    <form @submit.prevent="">
+      <email/>
+    </form>
+  </div>
+</template>
+
+<script>
+import email from '@/components/input/email.vue';
+
+export default {
+  components: { email },
+};
+</script>
