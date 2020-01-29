23820831772d7d50a25d85728ceab2cbb4b53781
Jasey <Jwaegebaert@gmail.com>
2020-01-29 14:54:19 +0100

    search style

--- src/views/map/Show.vue
+++ src/views/map/Show.vue
@@ -1,5 +1,18 @@
+<style lang="scss">
+@import '@/assets/styles/main-light';
+
+.backgroundMap{
+    background-image: url('~@/assets/images/mapL.png');
+    background-repeat:  no-repeat;
+    background-attachment: fixed;
+    background-size:  cover;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
+
 <template>
-    <div>
+    <div class="backgroundMap">
         <search />
     </div>
 </template>
@@ -8,7 +21,7 @@
 import Search from './partials/Search.vue';
 
 export default {
-    name: 'map',
+    name: 'mapShow',
     components: { Search },
 
 };
