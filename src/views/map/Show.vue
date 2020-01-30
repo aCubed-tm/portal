c853da40dcda66e9dc92c8eb8bdd39f178c4fc9d
Jasey <Jwaegebaert@gmail.com>
2020-01-30 10:12:00 +0100

    Refined search

--- src/views/map/Show.vue
+++ src/views/map/Show.vue
@@ -13,35 +13,55 @@
 
 <template>
     <div class="backgroundMap">
-        <div class="row ml-5 mt-5 position-fixed">
-            <search @show-result="showResult"/>
+        <div class="row ml-5 mt-5 position-fixed"
+            v-on-click-outside="stopSearch">
+            <div
+            @click="startSearch">
+                <search @show-result="showResult" :active="active"/>
+            </div>
             <transition name="fadeX">
-                <result v-if="result" class="ml-3"  @hide-result="hideResult"/>
+                <result v-if="object"
+                        class="ml-3"
+                        :object="object"
+                        @hide-result="hideResult"/>
             </transition>
         </div>
     </div>
 </template>
 
 <script>
+import { mixin as onClickOutside } from 'vue-on-click-outside';
 import Search from './partials/Search.vue';
 import Result from './partials/Results.vue';
 
 export default {
+    mixins: [onClickOutside],
     name: 'map',
     components: { Search, Result },
     data() {
         return {
             result: false,
+            object: false,
+            active: false,
         };
     },
     methods: {
-      showResult(id) {
-        console.log(`selected: ${ id }`);
-        this.result = true;
+      showResult(object) {
+        this.object = object;
       },
       hideResult() {
-        this.result = false;
+        this.active = false;
+        this.object = false;
+      },
+      stopSearch() {
+        this.active = false;
+        this.object = false;
+      },
+      startSearch() {
+          this.active = true;
       },
     },
 };
+
+
 </script>
--- src/views/map/partials/Results.vue
+++ src/views/map/partials/Results.vue
@@ -8,33 +8,37 @@
 <template>
     <div class="card" style="width: 32rem;">
         <div id="searchResults" class="card-body">
-            <h5 class="card-title">Vehicle information for <span class="font-weight-bold">{{vehicle.name}}</span></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Latest update at: {{latest.time}}</h6>
+            <button type="button" class="close" aria-label="Close"
+                    @click="hideResult">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <h5 class="card-title">{{object.type}} information for
+                <span class="font-weight-bold">{{object.name}}</span>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">Latest update at: 30/01/2020</h6>
             <br/><br/>
             <h5 class="font-weight-bold">Description</h5>
-            <p class="fontSmaller">{{vehicle.description}}</p>
+            <p class="fontSmaller">{{object.note}}</p>
             <hr/>
             <div class="row">
                 <div class="col-4">
-                    <button class="btn btn-primary"
-                    @click="hideResult">Get directions</button>
+                    <button class="btn btn-primary">Get directions</button>
                 </div>
                 <div class="col-8 fontSmaller">
-                    Vehicle {{vehicle.name}} was located
-                    [now - vehicle.location.latest.time => to seconds]
-                    seconds ago at {{latest.lat}} - {{latest.long}}
+                    {{object.type}} {{object.name}} was located
+                    {{object.timestamps[0].timestamp}} at
+                    {{object.timestamps[0].location}}
                 </div>
             </div>
             <hr/>
             <div>
                 <h5 class="font-weight-bold">History</h5>
                 <ul>
-                    <li v-for="item in vehicle.locations.slice().reverse()" :key="item.id">
-                        16 seconds ago: {{item.lat}}, {{item.long}}
+                    <li v-for="timestamp in object.timestamps" :key="timestamp.id">
+                        {{timestamp.timestamp}}: {{timestamp.location}}
                     </li>
                 </ul>
             </div>
-
         </div>
     </div>
 </template>
@@ -42,42 +46,15 @@
 <script>
 export default {
     name: 'map-results',
-    data() {
-        return {
-            vehicle: {
-                name: 'TRA12X235',
-                // eslint-disable-next-line max-len
-                description: 'Er zijn vele variaties van passages van Lorem Ipsum beschikbaar maar het merendeel heeft te lijden gehad van wijzigingen in een of andere vorm, door ingevoegde humor of willekeurig gekozen woorden die nog niet half geloofwaardig ogen. ',
-                locations: [
-                    {
-                        lat: '53.123456',
-                        long: '4.234567',
-                        time: '12:34:23-000',
-                    },
-                    {
-                        lat: '53.8765',
-                        long: '4.234567',
-                        time: '12:34:24-000',
-                    },
-                ],
-            },
-        };
-    },
     props: {
-        id: {
-        default: '',
+        object: {
+            default: '',
         },
     },
     methods: {
         hideResult() {
             this.$emit('hide-result');
         },
-    },
-
-    computed: {
-        latest() {
-            return this.vehicle.locations[this.vehicle.locations.length - 1];
-            },
     },
 };
 </script>
