8fbacfff5e6ec1edc11e9c469b71468f66eefbb6
Jasey <Jwaegebaert@gmail.com>
2020-02-13 20:09:40 +0100

    Detail page

--- src/views/map/partials/Detail.vue
+++ src/views/map/partials/Detail.vue
@@ -3,74 +3,80 @@
     font-size: 12px;
 }
 
-//! Tablet
-@media screen and (max-width: 720px) {
-  body {
-    .card{
-        font-size: 90%;
-    }
-  }
+.fontBigger {
+    font-size: 23px;
 }
 
-//! Mobile
-@media screen and (max-width: 540px) {
-  body {
-    .card{
-        font-size: 85%;
+.colorHover{
+    :hover i {
+        color: #CF366D;
     }
-  }
+}
+
+.list-group-item {
+    background-color: transparent !important;
 }
 </style>
 
 <template>
-    <div class="card" style="max-width: 32rem;">
-        <div id="searchResults" class="card-body">
-            <button type="button" class="close" aria-label="Close"
-                    @click="hideResult">
-                <span aria-hidden="true">&times;</span>
+    <div id="searchResults" class="card-body">
+        <div class="colorHover" @click="hideResult">
+            <button>
+                <i class="fas fa-arrow-left"></i> Back
             </button>
-            <h5 class="card-title">{{object.type}} information for
-                <span class="font-weight-bold">{{object.name}}</span>
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted">Latest update at: 30/01/2020</h6>
-            <br/><br/>
-            <h5 class="font-weight-bold">Description</h5>
-            <p class="fontSmaller">{{object.note}}</p>
-            <hr/>
-            <div class="row">
-                <div class="col-md-6">
-                    <button class="btn btn-primary" @click="directionsObject">Get directions</button>
-                </div>
-                <div class="col-md-6 fontSmaller mt-2">
-                    {{object.type}} {{object.name}} was located
-                    {{object.timestamps[0].timestamp}} at
-                    {{object.timestamps[0].location}}
-                </div>
-            </div>
-            <hr/>
-            <div>
-                <h5 class="font-weight-bold">History</h5>
-                <ul>
-                    <li v-for="timestamp in object.timestamps" :key="timestamp.id">
-                        {{timestamp.timestamp}}: {{timestamp.location}}
-                    </li>
-                </ul>
-            </div>
+        </div>
+        <br/>
+        <h5>{{object.type}} information for: </h5>
+        <hr/>
+        <div>
+            <b class="fontBigger">{{object.name}}</b>
+            <live-icon
+                v-if="Math.abs(object.timestamps[0].timestamp.diff(time)) < 60000"
+                class="live-icon">
+            </live-icon>
+            <i v-else class="fas fa-exclamation-circle pl-2 small text-black-50"></i>
+        </div>
+        <br/>
+        <p class="fontSmaller">{{object.note}}</p>
+        <br/>
+        <p class="card-subtitle mb-2 text-muted">Last update:
+            <br/> {{ latestDate }}
+        </p>
+        <br/>
+        <div>
+            <h5>History</h5>
+            <ul class="list-group list-group-flush">
+                <li v-for="timestamp in object.timestamps"
+                :key="timestamp.id"
+                class="list-group-item">
+                    {{ moment(timestamp.timestamp).format('LLL') }}
+                    <br/><span class="fontSmaller">located at: {{timestamp.x}} {{timestamp.y}}</span>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
+import moment from 'moment';
+import LiveIcon from '@/components/LiveIcon.vue';
+
 export default {
     name: 'map-results',
+    components: { LiveIcon },
+    computed: {
+        latestDate() {
+            return moment(this.object.timestamps[0].timestamp).format('LLL');
+        },
+    },
     props: {
         object: {
             default: '',
         },
     },
     methods: {
-        directionsObject() {
-            this.$emit('directions-object');
+        moment(date) {
+            return moment(date);
         },
         hideResult() {
             this.$emit('hide-result');
